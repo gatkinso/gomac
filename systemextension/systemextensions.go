a85cf76aa3fa098b1d8df33ed1e4f63170606b37
Geoffrey Atkinson <geoffrey_atkinso@hotmail.com>
2023-12-09 10:34:16 -0500

    Initial comit for SE framework

--- systemextension/systemextensions.go
+++ systemextension/systemextensions.go
@@ -1,1 +1,104 @@
 package systemextensions
+
+import "net/url"
+
+// OSSystemExtensionErrorDomain represents the error domain for system extensions
+var OSSystemExtensionErrorDomain string
+
+// OSBundleUsageDescriptionKey represents the key for bundle usage description
+var OSBundleUsageDescriptionKey string
+
+// NSSystemExtensionUsageDescriptionKey represents the key for system extension usage description
+var NSSystemExtensionUsageDescriptionKey string
+
+// OSRelatedKernelExtensionKey represents the key for related kernel extension
+var OSRelatedKernelExtensionKey string
+
+// OSSystemExtensionErrorCode represents the error codes for system extensions
+type OSSystemExtensionErrorCode int
+
+const (
+	OSSystemExtensionErrorUnknown                         OSSystemExtensionErrorCode = 1
+	OSSystemExtensionErrorMissingEntitlement              OSSystemExtensionErrorCode = 2
+	OSSystemExtensionErrorUnsupportedParentBundleLocation OSSystemExtensionErrorCode = 3
+	OSSystemExtensionErrorExtensionNotFound               OSSystemExtensionErrorCode = 4
+	OSSystemExtensionErrorExtensionMissingIdentifier      OSSystemExtensionErrorCode = 5
+	OSSystemExtensionErrorDuplicateExtensionIdentifer     OSSystemExtensionErrorCode = 6
+	OSSystemExtensionErrorUnknownExtensionCategory        OSSystemExtensionErrorCode = 7
+	OSSystemExtensionErrorCodeSignatureInvalid            OSSystemExtensionErrorCode = 8
+	OSSystemExtensionErrorValidationFailed                OSSystemExtensionErrorCode = 9
+	OSSystemExtensionErrorForbiddenBySystemPolicy         OSSystemExtensionErrorCode = 10
+	OSSystemExtensionErrorRequestCanceled                 OSSystemExtensionErrorCode = 11
+	OSSystemExtensionErrorRequestSuperseded               OSSystemExtensionErrorCode = 12
+	OSSystemExtensionErrorAuthorizationRequired           OSSystemExtensionErrorCode = 13
+)
+
+// OSSystemExtensionReplacementAction represents the replacement actions for system extensions
+type OSSystemExtensionReplacementAction int
+
+const (
+	OSSystemExtensionReplacementActionCancel  OSSystemExtensionReplacementAction = iota
+	OSSystemExtensionReplacementActionReplace OSSystemExtensionReplacementAction = iota
+)
+
+// OSSystemExtensionRequestResult represents the result of a system extension request
+type OSSystemExtensionRequestResult int
+
+const (
+	OSSystemExtensionRequestCompleted               OSSystemExtensionRequestResult = iota
+	OSSystemExtensionRequestWillCompleteAfterReboot OSSystemExtensionRequestResult = iota
+)
+
+// OSSystemExtensionRequest represents a system extension request
+type OSSystemExtensionRequest struct {
+	delegate   OSSystemExtensionRequestDelegate
+	identifier string
+}
+
+// ActivationRequestForExtension creates an activation request for a system extension
+func ActivationRequestForExtension(identifier string, queue interface{}) *OSSystemExtensionRequest {
+	// Add implementation here
+	return &OSSystemExtensionRequest{}
+}
+
+// DeactivationRequestForExtension creates a deactivation request for a system extension
+func DeactivationRequestForExtension(identifier string, queue interface{}) *OSSystemExtensionRequest {
+	// Add implementation here
+	return &OSSystemExtensionRequest{}
+}
+
+// PropertiesRequestForExtension creates a properties request for a system extension
+func PropertiesRequestForExtension(identifier string, queue interface{}) *OSSystemExtensionRequest {
+	// Add implementation here
+	return &OSSystemExtensionRequest{}
+}
+
+// OSSystemExtensionProperties represents the properties of a system extension
+type OSSystemExtensionProperties struct {
+	URL                    *url.URL
+	BundleIdentifier       string
+	BundleVersion          string
+	BundleShortVersion     string
+	IsEnabled              bool
+	IsAwaitingUserApproval bool
+	IsUninstalling         bool
+}
+
+// OSSystemExtensionRequestDelegate is a protocol for handling system extension requests
+type OSSystemExtensionRequestDelegate interface {
+	RequestActionForReplacingExtension(existing OSSystemExtensionProperties, ext OSSystemExtensionProperties) OSSystemExtensionReplacementAction
+	RequestNeedsUserApproval()
+	RequestDidFinishWithResult(result OSSystemExtensionRequestResult)
+	RequestDidFailWithError(error error)
+	RequestFoundProperties(properties []OSSystemExtensionProperties)
+}
+
+// OSSystemExtensionManager is responsible for managing system extensions
+type OSSystemExtensionManager struct {
+	sharedManager *OSSystemExtensionManager
+}
+
+// SubmitRequest submits a system extension request
+func (manager *OSSystemExtensionManager) SubmitRequest(request OSSystemExtensionRequest) {
+	// Submit the request
+}

c0d9337a0b63cc51881006eb85015117f26e2bab
Geoffrey Atkinson <geoffrey_atkinso@hotmail.com>
2023-12-10 11:42:17 -0500

    system extension unit test stubs

---
+++ systemextensions/systemextensions_test.go
@@ -0,0 +1,22 @@
+package systemextensions
+
+import "testing"
+
+// ActivationRequestForExtension creates an activation request for a system extension
+func TestActivationRequestForExtension(t *testing.T) {
+
+}
+
+// DeactivationRequestForExtension creates a deactivation request for a system extension
+func TestDeactivationRequestForExtension(t *testing.T) {
+
+}
+
+// PropertiesRequestForExtension creates a properties request for a system extension
+func TestPropertiesRequestForExtension(t *testing.T) {
+
+}
+
+func TestSubmitRequest(t *testing.T) {
+
+}
